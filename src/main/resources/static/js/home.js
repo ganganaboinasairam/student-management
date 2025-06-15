27eea36b8c3deac38aef936d65a1434def9e599b
“ganganaboinasairam” <ganganaboinasairam@gmail.com>
2025-06-15 07:01:04 +0530

    test commit

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,32 +1,29 @@
 /**
- * 
+ *
  */
 
+function deleteStudent(id) {
+  //alert(id);
 
+  url = "../api/student/delete/" + id;
 
-
- function deleteStudent(id){
-	 //alert(id);
-	 
-	 url = "../api/student/delete/"+id;
-	 
-	 fetch(url,{
-		 method:"DELETE",
-		 
-	 }).then(response => {
-		 if (response.ok){
-			 return response.json().catch(() => ({}));
-		 }else{
-			 throw new Error ("Network response was not ok");
-		 }
-	 })
-	 .then(data => {
-		 console.log("sucess: ",data);
-		 alert("Record deleted sucessfully");
-		 location.reload();
-	 })
-	 .catch((error) => {
-		 console.log("Error", error);
-		 alert("Failed to delete record");
-	 })
- }
+  fetch(url, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json().catch(() => ({}));
+      } else {
+        throw new Error("Network response is not ok");
+      }
+    })
+    .then((data) => {
+      console.log("sucess: ", data);
+      alert("Record deleted sucessfully");
+      location.reload();
+    })
+    .catch((error) => {
+      console.log("Error", error);
+      alert("Failed to delete record");
+    });
+}
